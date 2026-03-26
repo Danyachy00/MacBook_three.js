be16721d97ab69b30b733977c8392f46e9a1a84b
Danyachy00 <lollolloxlove@gmail.com>
2026-03-26 16:12:50 +0100

    Update app changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
 
 const App = () => {
   return (
       <main>
           <NavBar />
           <Hero/>
+          <ProductViewer/>
       </main>
   );
 };
